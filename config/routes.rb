f3f91a505d6e57fa2c205bff7b65c9bcb6e5cecc
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-14 22:55:45 +0500

    Create Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
   devise_for :users
   root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
