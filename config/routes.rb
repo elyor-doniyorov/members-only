775ebc21d319f599a472f10581e71c72ebe9cd15
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-14 23:20:36 +0500

    Add view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: [:new, :create, :index]
   devise_for :users
   root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
